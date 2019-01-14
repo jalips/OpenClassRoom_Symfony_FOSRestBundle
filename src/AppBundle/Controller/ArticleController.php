d2f40f80c6f3468ed2c6e271cdb1dfffe3ccb26d
Unknown <jservice@protonmail.com>
2019-01-14 13:23:11 +0100

    2.3 : CreateRoute

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -4,8 +4,16 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Article;
 use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-class ArticleController
+class ArticleController extends FOSRestController
 {
     /**
      * @Get(
@@ -19,4 +27,27 @@ class ArticleController
     {
         return $article;
     }
+
+    /**
+     * @Rest\Post(
+     *    path = "/articles",
+     *    name = "app_article_create"
+     * )
+     * @Rest\View(StatusCode = 201)
+     * @ParamConverter("article", converter="fos_rest.request_body")
+     */
+    public function createAction(Article $article)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($article);
+        $em->flush();
+
+        return $this->view(
+            $article,
+            Response::HTTP_CREATED,
+            ['Location' => $this->generateUrl('app_article_show', ['id' => $article->getId(), UrlGeneratorInterface::ABSOLUTE_URL])]
+        );
+    }
+
 }
